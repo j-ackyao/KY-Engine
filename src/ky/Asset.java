039917379827c3b50642ab5aaebc018dc04ae68f
Jack <crowwastaken@users.noreply.github.com>
2021-06-27 10:36:50 -0700

    added more constructors to asset, added scaling to image rendering

--- src/ky/Asset.java
+++ src/ky/Asset.java
@@ -1,43 +1,170 @@
 package ky;
 
-import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Asset {
 	
 	int x;
 	int y;
-	Image image = null;
-	Image[] images = null;
+	int width;
+	int height;
+	BufferedImage image = null;
+	BufferedImage[] images = null;
 	String identifier = null;
 	boolean animated = false;
 	int animationCycle = -1;
 	
-	public Asset(Image image, int x, int y) {
+	public Asset(BufferedImage image, int x, int y) {
 		this.image = image;
 		this.x = x;
 		this.y = y;
+		this.width = image.getWidth();
+		this.height = image.getHeight();
 	}
 	
-	public Asset(Image image, int x, int y, String name) {
+	public Asset(BufferedImage image, int x, int y, int width, int height) {
+		this.image = image;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Asset(BufferedImage image, int x, int y, String name) {
 		this.identifier = name;
 		this.image = image;
 		this.x = x;
 		this.y = y;
+		this.width = image.getWidth();
+		this.height = image.getHeight();
 	}
 	
-	public Asset(Image[] images, int x, int y) {
+	public Asset(BufferedImage image, int x, int y, int width, int height, String name) {
+		this.identifier = name;
+		this.image = image;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Asset(BufferedImage[] images, int x, int y) {
 		this.animated = true;
 		this.images = images;
 		this.x = x;
 		this.y = y;
+		this.width = images[0].getWidth();
+		this.height = images[0].getHeight();
 	}
 	
-	public Asset(Image[] images, int x, int y, String identifier) {
+	public Asset(BufferedImage[] images, int x, int y, int width, int height) {
 		this.animated = true;
-		this.identifier = identifier;
 		this.images = images;
 		this.x = x;
 		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Asset(BufferedImage[] images, int x, int y, String name) {
+		this.animated = true;
+		this.identifier = name;
+		this.images = images;
+		this.x = x;
+		this.y = y;
+		this.width = images[0].getWidth();
+		this.height = images[0].getHeight();
+	}
+	
+	public Asset(BufferedImage[] images, int x, int y, int width, int height, String name) {
+		this.animated = true;
+		this.identifier = name;
+		this.images = images;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Asset(String filename, int x, int y) {
+		this.image = readImage(filename);
+		this.x = x;
+		this.y = y;
+		this.width = this.image.getWidth();
+		this.height = this.image.getHeight();
+	}
+	
+	public Asset(String filename, int x, int y, int width, int height) {
+		this.image = readImage(filename);
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Asset(String filename, int x, int y, String name) {
+		this.identifier = name;
+		this.image = readImage(filename);
+		this.x = x;
+		this.y = y;
+		this.width = this.image.getWidth();
+		this.height = this.image.getHeight();
+	}
+	
+	public Asset(String filename, int x, int y, int width, int height, String name) {
+		this.identifier = name;
+		this.image = readImage(filename);
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Asset(String[] filenames, int x, int y) {
+		this.animated = true;
+		this.images = readImage(filenames);
+		this.x = x;
+		this.y = y;
+		this.width = this.images[0].getWidth();
+		this.height = this.images[0].getHeight();
+	}
+	
+	public Asset(String[] filenames, int x, int y, int width, int height) {
+		this.animated = true;
+		this.images = readImage(filenames);
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Asset(String[] filenames, int x, int y, String name) {
+		this.animated = true;
+		this.identifier = name;
+		this.images = readImage(filenames);
+		this.x = x;
+		this.y = y;
+		this.width = this.images[0].getWidth();
+		this.height = this.images[0].getHeight();
+	}
+	
+	public Asset(String[] filenames, int x, int y, int width, int height, String name) {
+		this.animated = true;
+		this.identifier = name;
+		this.images = readImage(filenames);
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void rescale(double factor) {
+		this.width *= factor;
+		this.height *= factor;
 	}
 	
 	public String getName() {
@@ -67,11 +194,11 @@ public class Asset {
 		return 0;
 	}
 	
-	public Image getImage() {
+	public BufferedImage getImage() {
 		return this.image;
 	}
 	
-	public Image getImage(int index) {
+	public BufferedImage getImage(int index) {
 		try {
 			return this.images[index];
 		} catch (ArrayIndexOutOfBoundsException aioobe) {
@@ -80,7 +207,43 @@ public class Asset {
 		return null;
 	}
 
-	public Image[] getImages() {
+	public BufferedImage[] getImages() {
 		return this.images;
+	}
+	
+	
+	private BufferedImage readImage(String filename) {
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File(filename));
+		} catch (IOException e) {
+			e.printStackTrace();
+			try {
+				image = ImageIO.read(new File("missing.png"));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				System.exit(-1);
+			}
+		}
+		return image;
+	}
+	
+	private BufferedImage[] readImage(String[] filenames) {
+		BufferedImage[] images = new BufferedImage[filenames.length];
+		for(int i = 0; i < filenames.length; i++) {
+			try {
+				images[i] = ImageIO.read(new File(filenames[i]));
+			} catch (IOException e) {
+				e.printStackTrace();
+				try {
+					images[i] = ImageIO.read(new File("missing.png"));
+				} catch (IOException e1) {
+					e1.printStackTrace();
+					System.exit(-1);
+				}
+			}
+		}
+		
+		return images;
 	}
 }
