629b7ea5518a93e3078b8f1c0a2391ad115a9a1e
liver man <31680668+thyliverman@users.noreply.github.com>
2021-07-25 15:19:27 -0700

    begin entity inheritance system

--- src/ky/BoxCollider.java
+++ src/ky/BoxCollider.java
@@ -1,5 +1,14 @@
 package ky;
 
-public class BoxCollider {
+public class BoxCollider extends Entity {
+
+	public BoxCollider(double x, double y, int layer) {
+		super(x, y, layer);
+	}
+	
+	@Override
+	public void update(double deltaTime) {
+		System.out.println("Wow we are doing collision stuff");
+	}
 	
 }
